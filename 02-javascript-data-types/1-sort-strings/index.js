6042bdc535526393f04996ac847eb04f7fcbcd4d
Svetlana <svethope@mail.ru>
2024-09-20 22:49:25 +0300

    Update index.js
    Sort-string

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  arr.toLocaleString();
+  	let collator = new Intl.Collator({ sensitivity: "variant" }, { caseFirst: "upper" });
+  	arr.sort((a, b) => collator.compare(a, b));
+  	if (param === 'desc') {
+  		let collator = new Intl.Collator({ sensitivity: "variant" }, { caseFirst: "lower" });
+  		arr.sort((a, b) => collator.compare(b, a));
 }
