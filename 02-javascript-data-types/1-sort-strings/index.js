13a322a01042c5643503165634ba72c21d679ea3
Svethope <sv.podlevskih@gmail.com>
2024-09-21 15:53:45 +0300

    sort strings update 1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,15 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+function sortStrings(arr, param = 'asc') {
+	arr.toLocaleString();
+	let collator = new Intl.Collator({ sensitivity: "variant" }, { caseFirst: "upper" });
+	arr.sort((a, b) => collator.compare(a, b));
+	if (param === 'desc') {
+		arr.reverse()
+	}
+	return (arr);
 }
+let arr = prompt("Massive, please: ").replace(/'/g, '"').split(', ');
+let param = prompt("Method of sort: ");
+alert(sortStrings(arr, param.toLocaleString()));
