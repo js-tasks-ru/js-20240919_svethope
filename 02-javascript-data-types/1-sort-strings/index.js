700b8a0d186a7d69b537d08eaf8fb0b13681b3f4
Svethope <sv.podlevskih@gmail.com>
2024-09-22 13:14:10 +0300

    sort-strings: Last 5 version

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -8,8 +8,6 @@ export function sortStrings(arr, param = 'asc') {
 	const locales = ["ru", "en"];
 	const options = { sensitivity: "variant", caseFirst: "upper" };
 	const collator = new Intl.Collator(locales, options);
-	let sortedArray = arr.slice();
-	sortedArray.sort((a, b) => collator.compare(a, b));
-	if (param === 'desc') { sortedArray.sort((a, b) => collator.compare(b, a)); }
-	return (sortedArray);
+	const sortFn = param === 'desc' ? (a, b) => collator.compare(b, a) : (a, b) => collator.compare(a, b);
+	return arr.slice().sort(sortFn);
 }
