620f65b5343b3ef1b92e819d3a929846973f360e
Svethope <sv.podlevskih@gmail.com>
2024-10-23 15:28:21 +0300

    06-enent-practice(tooltip)

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,72 @@
 class Tooltip {
-  initialize () {
+	static instance;
 
-  }
+	initialize() {
+		this.createListeners();
+	}
+
+	constructor() {
+		if (!Tooltip.instance) {
+			Tooltip.instance = this;
+		}
+		return Tooltip.instance;
+		this.element = null;
+	}
+
+	render(innertext) {
+		const element = document.createElement('div');
+		element.className = 'tooltip';
+		element.innerHTML = innertext;
+		document.body.append(element);
+		this.element = element;
+	}
+
+	handlerShow = (e) => {
+		document.addEventListener('pointerout', this.handlerOut);
+		document.addEventListener('pointermove', this.handlerMove);
+
+		if (this.isToolTip(e)) {
+			this.render(e.target.dataset.tooltip)
+		}
+	}
+
+	handlerMove = (e) => {
+		this.element.style.left = e.clientX + 10 + 'px';
+		this.element.style.top = e.clientY + 10 + 'px';
+	}
+
+	handlerOut = (e) => {
+		document.removeEventListener('pointermove', this.handlerShow);
+		document.removeEventListener('pointerout', this.handlerOut);
+
+		if (this.isToolTip(e) && this.element) {
+			this.remove();
+		}
+	}
+
+	isToolTip(e) {
+		return Boolean(e.target.dataset.tooltip)
+	}
+
+	destroy() {
+		this.destroyListeners();
+		this.remove();
+	}
+
+	remove() {
+		if (this.element) {
+			this.element.remove();
+		}
+	}
+
+	destroyListeners() {
+		document.removeEventListener('pointerover', this.handlerShow);
+	}
+
+	createListeners() {
+		document.addEventListener('pointerover', this.handlerShow);
+	}
+
 }
 
 export default Tooltip;
